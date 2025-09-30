b988d996917d9ef216cafa457829e03c0fa20629
Laudkyle <kyleaby1@gmail.com>
2025-09-30 14:56:32 +0000

    making error corrections on reservations

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require('./app');
-const { pool } = require('./config/db');
+const pool  = require('./config/db');
 
 const PORT = process.env.PORT || 5000;
 const swaggerUi = require("swagger-ui-express");
