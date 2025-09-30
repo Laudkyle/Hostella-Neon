24b547727397e121ad21a79fba58f77b74dada0e
Laudkyle <kyleaby1@gmail.com>
2025-09-30 09:10:25 +0000

    added more error checks on middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -18,7 +18,8 @@ module.exports = async (req, res, next) => {
   if (!token) {
     return res.status(401).json({ 
       success: false,
-      message: 'Not authorized to access this route' 
+      message: 'Not authorized to access this route' ,
+      error: "No token present"
     });
   }
 
